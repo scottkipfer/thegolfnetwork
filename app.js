e133413030ce1226cce2accf054f7aa36aa73edb
Scott Kipfer <scottkipfer@gmail.com>
2015-05-03 02:23:28 -0500

    added nodemailer capabilities

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ var passport = require('passport');
 var passport_config = require('./config/passport.js');
 var session = require('express-session');
 var jwt = require('express-jwt');
+// email
+var nodemailer = require('nodemailer');
+var mailgun = require('nodemailer-mailgun-transport');
+var mailauth = {
+    auth: {
+        api_key: process.env.MAILGUN_API_KEY,
+        domain: process.env.MAILGUN_DOMAIN_NAME
+    }
+}
+var transporter = nodemailer.createTransport(mailgun(mailauth));
+
+/*transporter.sendMail({
+    from:'Test <no-reply@thenetworkgolf.com>',
+    to:'scottkipfer@gmail.com',
+    subject:'this is a test',
+    html:'<b>Wow </b>',
+    test: 'not so wow'
+}, function(err,info){
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Response: ' + info);
+    }
+});*/
 
 // Connect to the Database
 require('./server/database/connect');
