f93b5e27db93adf36bb654f20f90a2d4f9096fef
Scott Kipfer <scottkipfer@gmail.com>
2015-05-03 10:56:52 -0500

    added serverside protection for conflicting tee times

--- server/controllers/tee_time.js
+++ server/controllers/tee_time.js
@@ -45,20 +45,36 @@ exports.read = function (req, res) {
 
 // Update Tee Time
 exports.update = function (req,res) {
-    console.log('WE GET HERE' + req);
     var tee_time = req.tee_time;
-
-    tee_time.time = req.body.time;
-    tee_time.golfers = req.body.golfers;
-
-    tee_time.save(function(err){
-        if(err){
-            return res.status(500).json({
-                error: 'Cannot update tee time'
-            });
+    var isFull = true;
+    var isRemoving = false;
+    req.body.golfers.forEach(function(golfer){
+        if (golfer.name === 'empty' && isFull === true) {
+            isRemoving = true;
         }
-        res.json(tee_time);
     });
+    tee_time.golfers.forEach(function (golfer) {
+        if (golfer.name === 'empty' && isFull === true) {
+            isFull = false;
+        }
+    });
+    if (isFull && !isRemoving) {
+        return res.status(500).json({
+            error: 'Tee Time is full'
+        });
+    } else {
+        tee_time.time = req.body.time;
+        tee_time.golfers = req.body.golfers;
+
+        tee_time.save(function (err) {
+            if (err) {
+                return res.status(500).json({
+                    error: 'Cannot update tee time'
+                });
+            }
+            res.json(tee_time);
+        });
+    }
 };
 
 // Delete Tee Time
