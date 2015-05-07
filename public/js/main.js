791225ed33c94dcd735d2b3f6cce9e92246facfd
Scott Kipfer <scottkipfer@gmail.com>
2015-05-06 23:42:55 -0500

    added sliding drawer

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,6 @@ var app = angular.module('the-network-golf', [
     'ui.router',
     'network-golf-controllers',
     'ngResource',
-    'snap',
     'tng-login',
     'tng-schedule',
     'tng-courses',
